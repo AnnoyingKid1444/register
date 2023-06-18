c7fd1f35a47b7d7b54c6185fded5b454960955bf
AnnoyingKid1444 <nikitaem139@gmail.com>
2023-06-19 01:15:30 +0300

    1

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import MessageBlock from "./Components/MessageBlock";
 import {Routes, Route} from 'react-router-dom';
 import SendMessage from "./Components/SendMessage";
 import MessageForm from "./Components/MessageForm";
-import Bgimg from "./img/YTIT.9cdba6f960dcfb10da0c.png";
 function App() {
 
   return (
     <div className="App">
 
-      <img src={Bgimg} alt="" className="bgImg"/>
-            <Routes>
+            {/* <Routes>
             <Route path="/" element={<SendMessage/>} />
             <Route path="/MessageForm" element={<MessageForm />} />
-            </Routes>
+            </Routes> */}
 
-            {/* <MessageBlock/> */}
+            <MessageBlock/>
 
     </div>
   );
